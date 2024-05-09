579e5da386ae2600512a74855bef77ea68e8fd50
Brenno Vilela <brenno.cauet@gmail.com>
2024-05-09 14:41:29 -0300

    feat: configuração inicial do projeto

---
+++ src/main/java/com/pickpayteste/PickpaytesteApplication.java
@@ -0,0 +1,13 @@
+package com.pickpayteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpaytesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpaytesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickpayteste/PickpaytesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickpayteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpaytesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
